77195f6ab40185eb732c3e8a19ec53a3de68fdbc
Bobby Maher <bobby.maher@icabbi.com>
2021-01-18 05:08:30 +0000

    footer css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -158,7 +158,7 @@
 
         </div>
     </footer>
-    
+
     <!-- the video popup box -->
 
     <div class="modal fade" id="video_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
